f5cd6f92ed25f77f05946e4eb80d33a7a9e6a5a3
TORESTO <therealtoresto@gmail.com>
2020-04-28 16:06:46 +0300

    creating

---
+++ 1-simple.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const EventEmitter = function() {
+    this.events = {}; // hash of array of function
+};
+
+EventEmitter.prototype.on = function(name, fn) {
+    const event = this.events[name];
+    if (event) event.push(fn);
+    else this.events[name] = [fn];
+};
+
+EventEmitter.prototype.emit = function(name, ...data) {
+    const event = this.events[name];
+    if (event) event.forEach(fn => fn(...data));
+};
+
+module.exports = EventEmitter;
